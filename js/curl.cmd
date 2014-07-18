c0db4fc26c7c8492709df61ef166c97fe43f177d
Chad Berkley <cberkley@flightstats.com>
2014-07-18 10:35:35 -0700

    reorganizing controllers

--- js/curl.cmd
+++
@@ -1,1 +0,0 @@
-curl -v --header "Upgrade: websocket" --header "Connection: Upgrade" --header "Sec-WebSocket-Key: dIhlIHNhbXBsZSBub25jZQ==" --header "Sec-WebSocket-Version: 13" http://127.0.0.1:6437/v3.json
--- js/puffer-test.js
+++
@@ -1,4 +0,0 @@
-var pc = require('./puffer-controller');
-
-pc.puffMulti(1,1,0,0,0);
-
